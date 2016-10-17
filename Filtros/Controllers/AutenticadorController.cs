707793dbb6eadc40d56b61e2106efb0df714d918
Junior Silva <juniorpsilva7@gmail.com>
2016-10-17 15:59:23 -0200

    Commit Creation

---
+++ Filtros/Controllers/AutenticadorController.cs
@@ -0,0 +1,48 @@
+﻿using Filtros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filtros.Controllers
+{
+    public class AutenticadorController : Controller
+    {
+        //
+        // GET: /Autenticador/
+
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        public ActionResult Entrar(Usuario usuario) 
+        { 
+            if (usuario.Username != null && usuario.Password != null && 
+                usuario.Username.Equals("K19") && usuario.Password.Equals("K19"))
+            {
+                //Session["Usuario"] = usuario;
+                FormsAuthentication.SetAuthCookie(usuario.Username, false);
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                ViewBag.Mensagem = "Usuário  ou Senha incorreta!";
+                return View("Formulario");
+            }
+        }
+
+        public ActionResult Sair()
+        {
+            //Session.Abandon();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Formulario");
+        }
+
+
+
+
+    }
+}
---
+++ Filtros/Controllers/ProdutoController.cs
@@ -0,0 +1,50 @@
+﻿using Filtros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filtros.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        private K19Context db = new K19Context();
+        //
+        // GET: /Produto/
+
+        public ActionResult Index()
+        {
+            return View(db.Produtos.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Cadastrar()
+        {
+            if (Session["Usuario"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                TempData["Mensagem"] = "Acesso não permitido.";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produtos.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+
+    }
+}
